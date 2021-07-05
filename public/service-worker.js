ed6534527599f3e278a3ab4fbe1caab683602434
Johnny <jaani.matharu@gmail.com>
2021-07-04 22:57:27 -0400

    testing2

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,20 +9,20 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 //This part needs revision as files to cache we have to choose
 
 const FILES_TO_CACHE = [
-  '/',
-  'index.html',
-  'manifest.json',
-  'css/style.css',
-  'icons/icon-72x72.png',
-  'icons/icon-96x96.png',
-  'icons/icon-128x128.png',
-  'icons/icon-144x144.png',
-  'icons/icon-152x152.png',
-  'icons/icon-192x192.png',
-  'icons/icon-384x384.png',
-  'icons/icon-512x512.png',
-  'js/idb.js',
-  'js/index.js'
+
+  './index.html',
+  './manifest.json',
+  './css/style.css',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './js/idb.js',
+  './js/index.js'
 ];
 
 // Install the service worker
